cbbfcbabd5dc23f175c50221d3a555d0664fc6c7
Samuel Wahome <samuelwahome.k5@gmail.com>
2024-02-11 22:51:10 +0300

    ci: install swift dependencies

--- src-tauri/src/display.rs
+++ src-tauri/src/display.rs
@@ -45,7 +45,6 @@ pub fn create_app<R: Runtime>(app: AppState, builder: tauri::Builder<R>) -> taur
 
                 let nswindow = win.ns_window().unwrap();
 
-                #[cfg(not(test))]
                 unsafe { set_transparent_titlebar(&nswindow)};
             }
 
